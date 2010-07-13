a3a96d9a2bed796cd2d05380a712e2a801547ad9
Joshua Sierles <jsierles@gmail.com>
2010-07-13 17:46:52 +0200

    update to chef 0.9. Prefer nginx/unicorn over passenger. Too many other changes to mention here.

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,10 @@
-maintainer        "37signals"
-maintainer_email  "sysadmins@37signals.com"
-description       "Configures java"
-version           "0.1"
+maintainer        "Opscode, Inc."
+maintainer_email  "cookbooks@opscode.com"
+license           "Apache 2.0"
+description       "Installs java"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version           "0.10.0"
+
+%w{ debian ubuntu redhat centos fedora }.each do |os|
+  supports os
+end
